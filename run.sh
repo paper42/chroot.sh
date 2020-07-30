c3424dc6faee3a1e264812055c1adccfa87226cf
Michal Vasilek <michal@vasilek.cz>
2020-07-30 19:06:40 +0200

    Add fish, zsh, loksh and ksh support

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ mount -t tmpfs -o size=1G tmpfs "$chroot_dir/tmp"
 PATH='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'
 PS1='$(pwd): '
 shell=
-for s in bash mksh ash sh; do
+for s in zsh fish bash loksh ksh mksh ash sh; do
 	if [ -e "$chroot_dir/bin/$s" ]; then
 		shell="$s"
 		break
