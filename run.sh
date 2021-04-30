f6fbea0691cd2df4e435e4d4e69fa47181124e45
Michal Vasilek <michal@vasilek.cz>
2021-04-30 14:00:39 +0200

    support running a command, support doas

--- run.sh
+++ run.sh
@@ -5,11 +5,16 @@
 #
 # any argument to run.sh will be passed to the shell interpreter
 # example: ./run.sh -c ls
+SUDO=sudo
+if command -v doas >/dev/null; then
+    SUDO=doas
+fi
 
-if ! [ $(whoami) = "root" ]; then
-	sudo sh "$0" "$@"
+if ! [ "$(id -u)" -eq 0 ]; then
+	$SUDO sh "$0" "$@"
 	exit 0
 fi
+cmd="$*"
 cd "$(dirname "$0")" || exit 1
 oldPS1=$PS1
 oldPATH=$PATH
@@ -21,17 +26,20 @@ mount -o bind /dev "$chroot_dir/dev"
 mount -t tmpfs -o size=1G tmpfs "$chroot_dir/tmp"
 
 PATH='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'
-shell=
-for s in zsh fish bash loksh ksh mksh ash sh; do
-	if [ -f "$chroot_dir/bin/$s" ] || [ -L "$chroot_dir/bin/$s" ]; then
-		shell="$s"
-		break
-	fi
-done
+if ! [ "$cmd" ]; then
+    shell=
+    for s in zsh fish bash loksh ksh mksh ash sh; do
+        if [ -f "$chroot_dir/bin/$s" ] || [ -L "$chroot_dir/bin/$s" ]; then
+            shell="$s"
+            break
+        fi
+    done
+    cmd="/bin/$shell"
+fi
 
 PS1='\w: '
 [ "$shell" = "zsh" ] && PS1='%~: '
-PS1="$PS1" TERM=xterm-256color chroot "$chroot_dir" "/bin/$shell" $@
+PS1="$PS1" TERM=xterm-256color chroot "$chroot_dir" $cmd
 PATH=$oldPATH
 PS1="$oldPS1"
 
