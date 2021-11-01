2e26bfb80240b202ab1ad724ff552adb1b1091f5
Michal Vasilek <michal@vasilek.cz>
2021-11-01 16:26:03 +0100

    specifies a chroot directory

--- chroot.sh
+++ chroot.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # usage:
-# 1. create directory root/ with rootfs
-# 2. ./run.sh
+# 1. create directory with rootfs
+# 2. run chroot.sh <directory> [CMD [ARGS...]]
 #
-# any argument to run.sh will be ran inside the chroot
-# example: ./run.sh ls -la
+# if CMD is specified, this command will run in the chroot
+# if CMD is not specified, the most suitable shell will be selected and ran
+# example: chroot.sh void # runs a shell in void (probably bash)
+#          chroot.sh ubuntu ls -la # runs ls -la in an ubuntu chroot
 SUDO=sudo
 if command -v doas >/dev/null; then
     SUDO=doas
@@ -14,7 +16,12 @@ if ! [ "$(id -u)" -eq 0 ]; then
 	exec $SUDO sh "$0" "$@"
 fi
 
-chroot_dir='root'
+chroot_dir="$1"
+if [ -z "$chroot_dir" ]; then
+    echo "usage: $0 <directory>"
+    exit 1
+fi
+shift
 if ! [ -d "$chroot_dir" ]; then
     echo "chroot directory $chroot_dir does not exist"
     exit 1
@@ -31,7 +38,7 @@ mount -o bind /dev "$chroot_dir/dev"
 mount -t tmpfs -o size=1G tmpfs "$chroot_dir/tmp"
 
 PATH='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'
-if ! [ "$cmd" ]; then
+if [ -z "$cmd" ]; then
     shell=
     for s in zsh fish bash loksh ksh mksh ash sh; do
         if [ -f "$chroot_dir/bin/$s" ] || [ -L "$chroot_dir/bin/$s" ]; then
@@ -48,5 +55,5 @@ PS1="$PS1" TERM=xterm-256color chroot "$chroot_dir" $cmd
 PATH=$oldPATH
 PS1="$oldPS1"
 
-sleep .1
+sleep .1 # not posix
 umount "$chroot_dir/dev" "$chroot_dir/proc" "$chroot_dir/sys" "$chroot_dir/tmp"
