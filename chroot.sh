4b8766f8dfa7a7edf3ebd652a7416ac69db8a843
Michal Vasilek <michal@vasilek.cz>
2021-11-01 16:39:40 +0100

    add README.md

--- chroot.sh
+++ chroot.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-# usage:
-# 1. create directory with rootfs
-# 2. run chroot.sh <directory> [CMD [ARGS...]]
-#
-# if CMD is specified, this command will run in the chroot
-# if CMD is not specified, the most suitable shell will be selected and ran
-# example: chroot.sh void # runs a shell in void (probably bash)
-#          chroot.sh ubuntu ls -la # runs ls -la in an ubuntu chroot
+
 SUDO=sudo
 if command -v doas >/dev/null; then
     SUDO=doas
